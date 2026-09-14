ed9046e612f9c1138d5b99d2001aad2e13ed9fc7
tejaspillewar-cmyk <tejaspillewar@gmail.com>
2026-09-15 02:52:37 +0530

    working D02

--- PyLauncherPlugin/cPlugin.vb
+++ PyLauncherPlugin/cPlugin.vb
@@ -12,19 +12,28 @@ Namespace PyLauncherPlugin
         ''' <summary>Called by ETABS when the plugin menu item is clicked.</summary>
         Public Sub Main(ByRef SapModel As cSapModel, ByRef ISapPlugin As cPluginCallback)
             Try
-                ' Dynamically resolve the python file path relative to this plugin's DLL location
+                ' Get the folder where the DLL is located
                 Dim pluginFolder As String = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
-                Dim pythonFilePath As String = System.IO.Path.Combine(pluginFolder, "etabs_gui.pyw")                
+                
+                ' Look ONE folder up for the python file using ".."
+                Dim pythonFilePath As String = System.IO.Path.GetFullPath(System.IO.Path.Combine(pluginFolder, "..", "etabs_gui.pyw"))
+                
+                ' Check if the file actually exists before trying to run it
+                If Not System.IO.File.Exists(pythonFilePath) Then
+                    System.Windows.Forms.MessageBox.Show("Could not find the Python script at:" & vbCrLf & pythonFilePath, "File Not Found", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error)
+                    Return
+                End If
+                
                 Dim startInfo As New ProcessStartInfo()
-                ' Ensure python is in your system PATH or provide the full path to python.exe
                 startInfo.FileName = "pythonw.exe"
                 startInfo.Arguments = """" & pythonFilePath & """"
-                startInfo.UseShellExecute = True
+                startInfo.UseShellExecute = False
+                startInfo.CreateNoWindow = True
                 
                 Process.Start(startInfo)
                 
             Catch ex As Exception
-                ' Failsafe block in case the python script fails to launch
+                System.Windows.Forms.MessageBox.Show("Failed to launch Python script: " & ex.Message, "Plugin Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error)
             End Try
             
             ' Signal ETABS that plugin finished
@@ -32,7 +41,7 @@ Namespace PyLauncherPlugin
                 ISapPlugin.Finish(0)
             End If
         End Sub
-
+        
         ''' <summary>Returns plugin description text to ETABS.</summary>
         Public Function Info(ByRef Text As String) As Long
             Text = "Python Script Launcher Plugin"
